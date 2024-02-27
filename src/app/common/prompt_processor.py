37dadd9b75f1f2f54a56906a047aacae63b4872d
akpg <arunkumarpg@hamaraindustries.xyz>
2024-02-27 23:07:36 +0530

    feat: added common helper functions for messages and token count.

--- src/app/common/prompt_processor.py
+++ src/app/common/prompt_processor.py
@@ -20,6 +20,43 @@ def get_completion(prompt: str, model: str = "gpt-4-turbo-preview") -> str | Non
     return response.choices[0].message.content
 
 
+def get_completion_from_messages(
+    messages: list[dict],
+    model: str = "gpt-4-turbo-preview",
+    temperature: int = 0,
+    max_tokens: int = 500,
+) -> str | None:
+    response = OPENAI_CLIENT.chat.completions.create(
+        model=model,
+        messages=messages,  # type: ignore
+        temperature=temperature,
+        max_tokens=max_tokens,
+    )
+    return response.choices[0].message.content
+
+
+def get_completion_from_messages_and_token_count(
+    messages: list[dict],
+    model: str = "gpt-4-turbo-preview",
+    temperature: int = 0,
+    max_tokens: int = 500,
+) -> tuple:
+    response = OPENAI_CLIENT.chat.completions.create(
+        model=model,
+        messages=messages,  # type: ignore
+        temperature=temperature,
+        max_tokens=max_tokens,
+    )
+    content = response.choices[0].message.content
+    token_dict = {
+        "prompt_tokens": response.usage.prompt_tokens,  # type: ignore
+        "completion_tokens": response.usage.completion_tokens,  # type: ignore
+        "total_tokens": response.usage.total_tokens,  # type: ignore
+    }
+
+    return content, token_dict
+
+
 def process_prompt(prompt: str, model: str = "gpt-4-turbo-preview") -> None:
     """Process prompt and print response."""
     print("PROMPT:")
@@ -27,4 +64,30 @@ def process_prompt(prompt: str, model: str = "gpt-4-turbo-preview") -> None:
     print("*" * 100)
     print("API RESPONSE:")
     response = get_completion(prompt)
+    print(response)
+
+
+def process_messages_prompt(
+    messages: list[dict], model: str = "gpt-4-turbo-preview", temperature: int = 0
+) -> None:
+    print("MESSAGES:")
+    print(messages)
+    print("*" * 100)
+    print("API RESPONSE:")
+    response = get_completion_from_messages(
+        messages=messages, model=model, temperature=temperature
+    )
+    print(response)
+
+
+def process_messages_prompt_with_token_count(
+    messages: list[dict], model: str = "gpt-4-turbo-preview", temperature: int = 0
+) -> None:
+    print("MESSAGES:")
+    print(messages)
+    print("*" * 100)
+    print("API RESPONSE:")
+    response = get_completion_from_messages_and_token_count(
+        messages=messages, model=model, temperature=temperature
+    )
     print(response)
